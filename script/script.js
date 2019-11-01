79ca11ff18678be6482ce2a7b1153fcda1febf6c
Kin Au <kinnamau@gmail.com>
2019-11-01 11:37:57 +0000

    removed commented out code relates #51 #52

--- script/script.js
+++ script/script.js
@@ -5,9 +5,7 @@ window.addEventListener('scroll', showToTopBtn);
 function showToTopBtn() {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         toTopBtn.style.display = "block";
-        // toTopBtn.style.opacity = 1;
       } else {
         toTopBtn.style.display = "none";
-        // toTopBtn.style.opacity = 0;
       }
 }
